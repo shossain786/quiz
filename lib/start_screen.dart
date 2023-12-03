87070a969be6f01e5870e4f0d4efea5ba2422c20
Saddam <sddmhossain@786gmail.com>
2023-12-03 21:38:08 +0530

    script aupdated

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -14,6 +14,7 @@ class StartScreen extends StatelessWidget {
             child: Image.asset(
               'assets/images/quiz-logo.png',
               width: 300,
+              color: Colors.limeAccent,
             ),
           ),
           const SizedBox(height: 30),
